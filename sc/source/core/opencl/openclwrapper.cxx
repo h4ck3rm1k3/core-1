db1a7d3b2b1e98f87a76f9c7ecd7581e4e12bbe6
Michael Meeks <michael.meeks@suse.com>
2013-07-08 21:49:31 +0100

    try harder to setup the kernel environment.

--- sc/source/core/opencl/openclwrapper.cxx
+++ sc/source/core/opencl/openclwrapper.cxx
@@ -2114,6 +2114,7 @@ double *OclCalc::OclSimpleDeltaOperation(OpCode eOp, const double *pOpArray,
                                          const double *pSubtractSingle, size_t nElements)
 {
     KernelEnv kEnv;
+    SetKernelEnv(&kEnv);
 
     // select a kernel: cut & paste coding is utterly evil.
     const char *kernelName;
