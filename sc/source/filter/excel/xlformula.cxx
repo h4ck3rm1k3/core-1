2ce9380ad1cac8b3cb741dabcf11e91c4ff37d75
Kohei Yoshida <kohei.yoshida@gmail.com>
2013-08-09 12:09:44 -0400

    Export shared formulas to xls, from the new shared formula in the core.
    Turns out we don't export shared formulas to xlsx, so no work is needed
    there.
    
    Change-Id: I85604f3b80c95c5b6179bbdac41e758c5de7854a

--- sc/source/filter/excel/xlformula.cxx
+++ sc/source/filter/excel/xlformula.cxx
@@ -759,13 +759,6 @@ void XclTokenArrayHelper::ConvertStringToList( ScTokenArray& rScTokArr, sal_Unic
     }
 }
 
-// shared formulas ------------------------------------------------------------
-
-const ScTokenArray* XclTokenArrayHelper::GetSharedFormula( const XclRoot& /*rRoot*/, const ScTokenArray& /*rScTokArr*/ )
-{
-    return NULL;
-}
-
 // multiple operations --------------------------------------------------------
 
 namespace {
