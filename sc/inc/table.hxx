0a61d4af7502f1e0274b71cda0b40778ab51d742
Kohei Yoshida <kohei.yoshida@collabora.com>
2013-09-09 23:14:19 -0400

    Trim data array length to remove trailing empty rows.
    Change-Id: I61a6a289ad1c2c757fcea490ada5d40fee08e840

--- sc/inc/table.hxx
+++ sc/inc/table.hxx
@@ -470,6 +470,8 @@ public:
     bool        ShrinkToUsedDataArea( bool& o_bShrunk, SCCOL& rStartCol, SCROW& rStartRow,
                                       SCCOL& rEndCol, SCROW& rEndRow, bool bColumnsOnly ) const;
 
+    SCROW GetLastDataRow( SCCOL nCol1, SCCOL nCol2 ) const;
+
     SCSIZE      GetEmptyLinesInBlock( SCCOL nStartCol, SCROW nStartRow,
                                         SCCOL nEndCol, SCROW nEndRow, ScDirection eDir ) const;
 
