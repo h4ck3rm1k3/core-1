1690b769722a3357e3bea18c86f12175c67a6b1b
Markus Mohrhard <markus.mohrhard@googlemail.com>
2013-12-09 07:40:23 +0100

    initial work on showing the bitmap in Libreoffice
    Change-Id: Id6543889509982bdb7487de484cf3d7f79c15f1e

--- chart2/source/view/main/AbstractShapeFactory.cxx
+++ chart2/source/view/main/AbstractShapeFactory.cxx
@@ -88,6 +88,7 @@ AbstractShapeFactory* AbstractShapeFactory::getOrCreateShapeFactory(uno::Referen
             {
 
                 pShapeFactory = reinterpret_cast<__getOpenglShapeFactory>(fn)();
+                pShapeFactory->setShapeFactory(xFactory);
             }
         }
     }
